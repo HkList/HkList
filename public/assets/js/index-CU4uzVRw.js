596f75e2ba1e242a945c12f582b79db37a7a9347
huankong233 <huank0ng@foxmail.com>
2025-06-24 09:47:36 +0800

    [*] 修复开放平台

---
+++ public/assets/js/index-CU4uzVRw.js
@@ -0,0 +1,5 @@
+import{_ as o,a as r}from"./index-BOGZtms0.js";import{R as a}from"./index-BlUydGuh.js";/**
+ * tdesign v1.11.5
+ * (c) 2025 tdesign
+ * @license MIT
+ */var s=a(o);a(r);export{s as D};
